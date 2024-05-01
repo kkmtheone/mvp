c403e69235403dc7d93bf428d488fe03420d648d
kkimayu <kkm.theone@gmail.com>
2024-05-01 12:25:02 +0300

    correct use of react routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import SearchPage from './components/SearchPage';
@@ -14,13 +14,13 @@ const App = () => {
     <Router>
       <div>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/search" component={SearchPage} />
-          <Route path="/create" component={CreatePage} />
-          <Route path="/updates" component={UpdatesPage} />
-          <Route path="/profile" component={ProfilePage} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/updates" element={<UpdatesPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
       </div>
     </Router>
   );
